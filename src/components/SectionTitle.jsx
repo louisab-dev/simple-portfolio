9dd173a95eb39d2c1a441a3c20c46eea0b8a88bb
imlouislab <louisamorosbessede@gmail.com>
2022-10-04 18:46:24 +0200

    some tweaks to get everything inline

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function SectionTitle({children, id}) {
+function SectionTitle({ children, id }) {
   return (
     <h1
       id={id && id}
-      className="text-3xl font-bold mb-5 text-indigo-600 dark:text-indigo-500"
+      className="text-3xl font-bold mb-5 text-indigo-600 dark:text-indigo-500 mx-5"
     >
       {children}
     </h1>
--- src/components/items/SkillItem.jsx
+++ src/components/items/SkillItem.jsx
@@ -1,6 +1,6 @@
 function SkillItem({ title, icon, description }) {
   return (
-    <div className="bg-slate-300 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 hover:-translate-y-2 transform transition p-5 rounded-md h-[200px] mt-5 mx-5">
+    <div className="bg-slate-300 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 hover:-translate-y-2 transform transition p-5 rounded-md h-[250px] mt-5 mx-5">
       <div className="text-gray-700 dark:text-gray-300 w-6 h-6 mb-3">
         {icon}
       </div>
